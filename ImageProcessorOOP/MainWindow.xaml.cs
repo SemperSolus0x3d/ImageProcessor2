25802c8f7a60a6e79528aa74446c4e91791f96da
Andreyev Kyrylo <K.Andreyev.FIT.121.20@knute.edu.ua>
2021-11-14 23:23:11 +0200

    Implement GUI and MainWindow class

--- ImageProcessorOOP/MainWindow.xaml.cs
+++ ImageProcessorOOP/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Drawing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Media;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -20,9 +24,144 @@ namespace ImageProcessorOOP
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IEffect[] effects;
+        private History history;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeEffects();
+
+            history = new History();
+
+            ProcessingAlgorithmsListBox.ItemsSource = effects;
+        }
+
+        private void InitializeEffects()
+        {
+            effects = new IEffect[]
+            {
+                new CycleBitShiftLeft(),
+                new CycleBitShiftRight(),
+
+                new SwapRedAndGreen(),
+                new SwapGreenAndBlue(),
+                new SwapRedAndBlue()
+            };
+        }
+
+        private void LoadImage(Uri uri)
+        {
+            BitmapImage image = new BitmapImage(uri);
+
+            if (image.IsDownloading)
+            {
+                image.DownloadCompleted += OnImageDownloadCompleted;
+                image.DownloadFailed += (s, e) => MessageBox.Show("Download failed");
+                image.DecodeFailed += (s, e) => MessageBox.Show("Decode failed");
+            }
+            else
+                OnImageDownloadCompleted(image, null);
+        }
+
+        private void OnImageDownloadCompleted(object sender, EventArgs e)
+        {
+            history.Clear();
+            history.Save(new Image(sender as BitmapSource));
+
+            MainImage.BeginInit();
+            MainImage.Source = history.CurrentState.Bitmap;
+            MainImage.EndInit();
+        }
+
+        private void GridSplitter_MouseDoubleClick(object sender, RoutedEventArgs e)
+        {
+            MainGrid.RowDefinitions[5].Height =
+                new GridLength(MainGrid.RowDefinitions[5].MinHeight);
+        }
+
+        private void LoadImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Uri uri = null;
+
+            if (string.IsNullOrWhiteSpace(UriTextBox.Text))
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Title = "Выберите изображение";
+
+                if (dialog.ShowDialog() ?? false)
+                    uri = new Uri(dialog.FileName);
+                else return;
+            }
+            else
+                uri = new Uri(UriTextBox.Text);
+
+            UriTextBox.Text = uri.AbsoluteUri;
+
+            LoadImage(uri);
+        }
+
+        private void SaveImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (history.CurrentState == null)
+                return;
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "PNG Изображения|*.png|Все файлы|*.*";
+
+            if (dialog.ShowDialog() ?? false)
+            {
+                using (var file = dialog.OpenFile())
+                {
+                    BitmapEncoder encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(history.CurrentState.Bitmap));
+                    encoder.Save(file);
+                }
+            }
+
+            MessageBox.Show("Сохранено");
+        }
+
+        private void ProcessImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            IEffect effect = ProcessingAlgorithmsListBox.SelectedItem as IEffect;
+
+            var newImage = history.CurrentState.Clone();
+            effect.Apply(newImage);
+            history.Save(newImage);
+
+            MainImage.BeginInit();
+            MainImage.Source = newImage.Bitmap;
+            MainImage.EndInit();
+        }
+
+        private void MainWindow_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.None;
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                string[] files =
+                    e.Data.GetData(DataFormats.FileDrop, true) as string[];
+
+                if ((files?.Length ?? 0) == 1)
+                    e.Effects = DragDropEffects.All;
+            }
+        }
+
+        private void MainWindow_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                string[] files =
+                    e.Data.GetData(DataFormats.FileDrop, true) as string[];
+
+                if ((files?.Length ?? 0) == 1)
+                {
+                    UriTextBox.Text = files[0];
+                    LoadImage(new Uri(files[0]));
+                }
+            }
         }
     }
 }
