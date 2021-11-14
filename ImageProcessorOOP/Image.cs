55df039b3bf298d78d9f0115aa0cdf3977300e5d
Andreyev Kyrylo <K.Andreyev.FIT.121.20@knute.edu.ua>
2021-11-14 02:12:12 +0200

    Implement Image class

---
+++ ImageProcessorOOP/Image.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ImageProcessorOOP
+{
+    public class Image
+    {
+        public PixelFormat PreferredFormat { get; } 
+            = PixelFormats.Bgra32;
+
+        public WriteableBitmap Bitmap { get; private set; }
+
+        public Image(BitmapSource source)
+        {
+            // Create new Bitmap from source
+            // and convert format if needed
+            Bitmap = new WriteableBitmap(
+                source.Format == PreferredFormat ?
+                source :
+                ConvertFormatToPreferred(source)
+            );
+        }
+
+        public Image Clone()
+        {
+            return new Image(Bitmap.Clone());
+        }
+
+        private BitmapSource ConvertFormatToPreferred(
+            BitmapSource original
+        )
+        {
+            FormatConvertedBitmap converted = new FormatConvertedBitmap();
+
+            converted.BeginInit();
+            converted.Source = original;
+            converted.DestinationFormat = PreferredFormat;
+            converted.EndInit();
+
+            return converted;
+        }
+    }
+}
