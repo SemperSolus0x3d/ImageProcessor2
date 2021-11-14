a3f69ba59d353d7a4ea5f2e6b9a200d30430a926
Andreyev Kyrylo <K.Andreyev.FIT.121.20@knute.edu.ua>
2021-11-14 19:15:33 +0200

    Implement SwapRedAndGreen class

---
+++ ImageProcessorOOP/SwapRedAndGreen.cs
@@ -0,0 +1,17 @@
+﻿namespace ImageProcessorOOP
+{
+    public class SwapRedAndGreen : ComponentsSwap
+    {
+        public override string Name
+            => "Swap red and green components";
+
+        public override void Apply(Image image)
+        {
+            SwapPixelComponents(
+                image,
+                Component.Red,
+                Component.Green
+            );
+        }
+    }
+}
