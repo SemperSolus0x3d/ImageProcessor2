a80da73a63f57df5d25ab5009d40b7de7e3cf652
Andreyev Kyrylo <K.Andreyev.FIT.121.20@knute.edu.ua>
2021-11-15 00:21:58 +0200

    Wrap event handlers in try-catch blocks

--- ImageProcessorOOP/MainWindow.xaml.cs
+++ ImageProcessorOOP/MainWindow.xaml.cs
@@ -64,116 +64,172 @@ namespace ImageProcessorOOP
         // Ctrl+Z hotkey handler
         private void CancelHotkeyHandler(object sender, ExecutedRoutedEventArgs e)
         {
-            var source = history.GoBack();
-
-            if (source != null)
+            try
             {
-                MainImage.BeginInit();
-                MainImage.Source = source.Bitmap;
-                MainImage.EndInit();
+                var source = history.GoBack();
+
+                if (source != null)
+                {
+                    MainImage.BeginInit();
+                    MainImage.Source = source.Bitmap;
+                    MainImage.EndInit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
             }
         }
 
         // Ctrl+Shift+Z hotkey handler
         private void RevertHotkeyHandler(object sender, ExecutedRoutedEventArgs e)
         {
-            var source = history.GoForward();
-
-            if (source != null)
+            try
             {
-                MainImage.BeginInit();
-                MainImage.Source = source.Bitmap;
-                MainImage.EndInit();
+                var source = history.GoForward();
+
+                if (source != null)
+                {
+                    MainImage.BeginInit();
+                    MainImage.Source = source.Bitmap;
+                    MainImage.EndInit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
             }
         }
 
         private void GridSplitter_MouseDoubleClick(object sender, RoutedEventArgs e)
         {
-            MainGrid.RowDefinitions[5].Height =
-                new GridLength(MainGrid.RowDefinitions[5].MinHeight);
+            try
+            {
+                MainGrid.RowDefinitions[5].Height =
+                    new GridLength(MainGrid.RowDefinitions[5].MinHeight);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
         }
 
         private void LoadImageButton_Click(object sender, RoutedEventArgs e)
         {
-            Uri uri = null;
-
-            if (string.IsNullOrWhiteSpace(UriTextBox.Text))
+            try
             {
-                OpenFileDialog dialog = new OpenFileDialog();
-                dialog.Title = "Выберите изображение";
+                Uri uri = null;
 
-                if (dialog.ShowDialog() ?? false)
-                    uri = new Uri(dialog.FileName);
-                else return;
+                if (string.IsNullOrWhiteSpace(UriTextBox.Text))
+                {
+                    OpenFileDialog dialog = new OpenFileDialog();
+                    dialog.Title = "Выберите изображение";
+
+                    if (dialog.ShowDialog() ?? false)
+                        uri = new Uri(dialog.FileName);
+                    else return;
+                }
+                else
+                    uri = new Uri(UriTextBox.Text);
+
+                UriTextBox.Text = uri.AbsoluteUri;
+
+                LoadImage(uri);
             }
-            else
-                uri = new Uri(UriTextBox.Text);
-
-            UriTextBox.Text = uri.AbsoluteUri;
-
-            LoadImage(uri);
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
         }
 
         private void SaveImageButton_Click(object sender, RoutedEventArgs e)
         {
-            if (history.CurrentState == null)
-                return;
-
-            SaveFileDialog dialog = new SaveFileDialog();
-            dialog.Filter = "PNG Изображения|*.png|Все файлы|*.*";
-
-            if (dialog.ShowDialog() ?? false)
+            try
             {
-                using (var file = dialog.OpenFile())
-                {
-                    BitmapEncoder encoder = new PngBitmapEncoder();
-                    encoder.Frames.Add(BitmapFrame.Create(history.CurrentState.Bitmap));
-                    encoder.Save(file);
-                }
-            }
+                if (history.CurrentState == null)
+                    return;
 
-            MessageBox.Show("Сохранено");
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "PNG Изображения|*.png|Все файлы|*.*";
+
+                if (dialog.ShowDialog() ?? false)
+                {
+                    using (var file = dialog.OpenFile())
+                    {
+                        BitmapEncoder encoder = new PngBitmapEncoder();
+                        encoder.Frames.Add(BitmapFrame.Create(history.CurrentState.Bitmap));
+                        encoder.Save(file);
+                    }
+                }
+
+                MessageBox.Show("Сохранено");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
         }
 
         private void ProcessImageButton_Click(object sender, RoutedEventArgs e)
         {
-            IEffect effect = ProcessingAlgorithmsListBox.SelectedItem as IEffect;
+            try
+            {
+                IEffect effect = ProcessingAlgorithmsListBox.SelectedItem as IEffect;
 
-            var newImage = history.CurrentState.Clone();
-            effect.Apply(newImage);
-            history.Save(newImage);
+                var newImage = history.CurrentState.Clone();
+                effect.Apply(newImage);
+                history.Save(newImage);
 
-            MainImage.BeginInit();
-            MainImage.Source = newImage.Bitmap;
-            MainImage.EndInit();
+                MainImage.BeginInit();
+                MainImage.Source = newImage.Bitmap;
+                MainImage.EndInit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
         }
 
         private void MainWindow_DragOver(object sender, DragEventArgs e)
         {
-            e.Effects = DragDropEffects.None;
-
-            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            try
             {
-                string[] files =
-                    e.Data.GetData(DataFormats.FileDrop, true) as string[];
+                e.Effects = DragDropEffects.None;
 
-                if ((files?.Length ?? 0) == 1)
-                    e.Effects = DragDropEffects.All;
+                if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+                {
+                    string[] files =
+                        e.Data.GetData(DataFormats.FileDrop, true) as string[];
+
+                    if ((files?.Length ?? 0) == 1)
+                        e.Effects = DragDropEffects.All;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
             }
         }
 
         private void MainWindow_Drop(object sender, DragEventArgs e)
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            try
             {
-                string[] files =
-                    e.Data.GetData(DataFormats.FileDrop, true) as string[];
-
-                if ((files?.Length ?? 0) == 1)
+                if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
                 {
-                    UriTextBox.Text = files[0];
-                    LoadImage(new Uri(files[0]));
+                    string[] files =
+                        e.Data.GetData(DataFormats.FileDrop, true) as string[];
+
+                    if ((files?.Length ?? 0) == 1)
+                    {
+                        UriTextBox.Text = files[0];
+                        LoadImage(new Uri(files[0]));
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
             }
         }
     }
