450d378dcd5f004af052107eb162fa70bc77a51e
Andreyev Kyrylo <K.Andreyev.FIT.121.20@knute.edu.ua>
2021-11-14 03:04:39 +0200

    Implement CycleBitShiftLeft and CycleBitShiftRight classes

---
+++ ImageProcessorOOP/CycleBitShiftLeft.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace ImageProcessorOOP
+{
+    public class CycleBitShiftLeft : CycleBitShift
+    {
+        public override string Name => "Cycle bit shift left";
+
+        public unsafe override void Apply(Image image)
+        {
+            WriteableBitmap bitmap = image.Bitmap;
+
+            try
+            {
+                bitmap.Lock();
+
+                uint* bufferPtr = (uint*)bitmap.BackBuffer.ToPointer();
+                int pixelsCount = bitmap.PixelWidth * bitmap.PixelHeight;
+
+                for (int i = 0; i < pixelsCount; i++)
+                    bufferPtr[i] = ShiftBitsLeft(bufferPtr[i]);
+            }
+            finally
+            {
+                bitmap.Unlock();
+            }
+        }
+
+        private uint ShiftBitsLeft(uint a)
+        {
+            return (a << 1) | (a >> 31);
+        }
+    }
+}
---
+++ ImageProcessorOOP/CycleBitShiftRight.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace ImageProcessorOOP
+{
+    public class CycleBitShiftRight : CycleBitShift
+    {
+        public override string Name => "Cycle bit shift right";
+
+        public unsafe override void Apply(Image image)
+        {
+            WriteableBitmap bitmap = image.Bitmap;
+
+            try
+            {
+                bitmap.Lock();
+
+                uint* bufferPtr = (uint*)bitmap.BackBuffer.ToPointer();
+                int pixelsCount = bitmap.PixelWidth * bitmap.PixelHeight;
+
+                for (int i = 0; i < pixelsCount; i++)
+                    bufferPtr[i] = ShiftBitsRight(bufferPtr[i]);
+            }
+            finally
+            {
+                bitmap.Unlock();
+            }
+        }
+
+        private uint ShiftBitsRight(uint a)
+        {
+            return (a >> 1) | (a << 31);
+        }
+    }
+}
