f5bc805bf029d3897be31a9915d733501adcd68b
Andreyev Kyrylo <K.Andreyev.FIT.121.20@knute.edu.ua>
2021-11-14 02:54:24 +0200

    Create CycleBitShift abstract class

---
+++ ImageProcessorOOP/CycleBitShift.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageProcessorOOP
+{
+    public abstract class CycleBitShift : IEffect
+    {
+        public abstract string Name { get; }
+
+        public abstract void Apply(Image image);
+    }
+}
